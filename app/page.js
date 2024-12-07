92088aa8d54a6daf72e54bb33f6b5e9991bbcdbb
Dev Bandil <guptastock69@gmail.com>
2024-12-07 15:09:44 +0530

    done

--- app/page.js
+++ app/page.js
@@ -1,12 +1,18 @@
-import Hero from "./Home/page"
-import Navbar from "./_components/Navbar";
+"use client"
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
-  
+  const router = useRouter();
+
+  useEffect(() => {
+    // Redirect to '/home' when the page is loaded
+    router.push('/Home');
+  }, [router]);
+
+  // Optional, since the user will be redirected immediately
+
   return (
-    <div className=" text-white   ">
-   <Hero/>   
-   
-    </div>
+    null
   );
 }
